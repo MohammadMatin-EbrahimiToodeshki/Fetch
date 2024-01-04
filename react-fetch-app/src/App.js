b7f9e4079e37d9a3f55a0fff5b3453ab19dcaa59
MohammadMatin-EbrahimiToodeshki <92645903+MohammadMatin-EbrahimiToodeshki@users.noreply.github.com>
2024-01-04 20:57:43 +0000

    fetch

---
+++ react-fetch-app/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>JSON Data:</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
